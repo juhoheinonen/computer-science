d04362f7ba9a1ffd9fc49ddb932d7dde68e09319
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2019-12-21 12:05:00 +0200

    Improve bs.

--- src/ComputerScience/BinarySearcher.cs
+++ src/ComputerScience/BinarySearcher.cs
@@ -5,17 +5,37 @@ namespace ComputerScience
 {
     public class BinarySearcher
     {
-        public TestPerson SearchById(int id, IEnumerable<TestPerson> people)
+        public TestPerson SearchById(int id, IList<TestPerson> people)
         {
             if (people == null)
             {
                 return null;
             }
 
-            people = people.OrderBy(p => p.Id);
+            people = people.OrderBy(p => p.Id).ToList();
 
             int middleItemIndex = people.Count() / 2;
 
+            var foundPerson = people[middleItemIndex];
+
+            if (foundPerson.Id == id)
+            {
+                return foundPerson;
+            }
+
+            if (middleItemIndex == 0)
+            {
+                return null;
+            }
+
+            if (foundPerson.Id < id)
+            {
+                return SearchById(id, people.Skip(middleItemIndex).ToList());
+            }
+            else
+            {
+                return SearchById(id, people.Take(middleItemIndex).ToList());
+            }
         }
     }
 
--- tests/ComputerScienceTests/BinarySearchTests.cs
+++ tests/ComputerScienceTests/BinarySearchTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ComputerScience;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace ComputerScienceTests
 {
@@ -8,7 +10,47 @@ namespace ComputerScienceTests
         [TestMethod]
         public void Search_FindsItem()
         {
+            var people = new List<TestPerson>();
 
+            people.Add(new TestPerson()
+            {
+                Id = 5,
+                Name = "Tester 0"
+            });
+
+            people.Add(new TestPerson()
+            {
+                Id = 3,
+                Name = "Tester 1"
+            });
+
+            people.Add(new TestPerson()
+            {
+                Id = 2,
+                Name = "Tester 2"
+            });
+
+            var sut = new BinarySearcher();
+
+            var result = sut.SearchById(3, people);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Tester 1", result.Name);
+
+            result = sut.SearchById(5, people);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Tester 0", result.Name);
+
+            result = sut.SearchById(2, people);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Tester 2", result.Name);
+
+
+            result = sut.SearchById(999, people);
+
+            Assert.IsNull(result);            
         }
     }
 }
