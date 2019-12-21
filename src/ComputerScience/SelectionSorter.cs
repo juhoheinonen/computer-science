67ea079caf5c3594887570228010fa40b8d385a5
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2019-12-21 19:29:34 +0200

    Implement selection sort.

---
+++ src/ComputerScience/SelectionSorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerScience
+{
+    public class SelectionSorter : ISorter
+    {
+        public void Sort(int[] A)
+        {
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                var smallestIndex = i;
+                
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[j] < A[smallestIndex])
+                    {                        
+                        smallestIndex = j;
+                    }
+                }
+                int temp = A[i];
+                A[i] = A[smallestIndex];
+                A[smallestIndex] = temp;
+            }
+        }
+    }
+}
---
+++ tests/ComputerScienceTests/SelectionSortTests.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using ComputerScience;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace ComputerScienceTests
+{
+    [TestClass]
+    public class SelectionSortTests
+    {
+        [DataRow(2, 2, 3, 4)]
+        [DataRow(3, 2, 1)]
+        [DataRow(3, 2, 4, 1, 0, 5, 5)]
+        [DataRow(-4, 2, 0, 0, 1, 5, 99, 123, 4, 5)]
+        [DataTestMethod]
+        public void SelectionSort_DifferentInputs_ArrayIsSorted(params int[] inputArray)
+        {
+            var sortedByBuiltinSort = new int[inputArray.Length];
+            Array.Copy(inputArray, sortedByBuiltinSort, sortedByBuiltinSort.Length);
+            Array.Sort(sortedByBuiltinSort);
+
+            var sut = new SelectionSorter();
+
+            sut.Sort(inputArray);
+
+            Assert.IsTrue(sortedByBuiltinSort.SequenceEqual(inputArray));
+        }
+
+        [TestMethod]
+        public void SelectionSort_RandomInput_ArrayIsSorted()
+        {
+            var faker = new Faker();
+
+            int[] inputArray = new int[faker.Random.Int(10, 10000)];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = faker.Random.Int();
+            }
+
+            var sortedByBuiltinSort = new int[inputArray.Length];
+            Array.Copy(inputArray, sortedByBuiltinSort, sortedByBuiltinSort.Length);
+            Array.Sort(sortedByBuiltinSort);
+
+            var sut = new SelectionSorter();
+
+            sut.Sort(inputArray);
+
+            Assert.IsTrue(sortedByBuiltinSort.SequenceEqual(inputArray));
+        }
+    }
+}
